6555c7a7bd05ec6298a89cd903178f650febb09d
Eric Smith <26430018+ericsmi@users.noreply.github.com>
2024-09-05 11:10:47 -0700

    quick check before TO

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,15 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Badly probably.
+
+Use a positive edge detector on the clock and its compliment.  Or together those dectors to get 2 positive pulses per period or a 2x clock.  Vary clk 2x pos pulse width by varying number of inv per detect. 
 
 ## How to test
 
-Explain how to use your project
+Carefully.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+Analog Discovery 3
+
--- src/project.v
+++ src/project.v
@@ -6,21 +6,26 @@
 
 `default_nettype wire
 
-module delaygate(input A, output Z);
+module invgate(input A, output Y);
  `ifdef COCOTB_SIM
-   assign #5 Z = A; 
+    assign #1 Y = ~A;
  `else
-    localparam N=4; // must be an even number greater than 2
-    wire [N-2:0] X;
-    wire [N-1:0] Y;
-    sky130_fd_sc_hd__inv_2 [N-1:0] inv(.A({X[N-2:0],A}),.Y(Y[N-1:0]));
-    assign Z=Y[N-1];
+    sky130_fd_sc_hd__inv_2 inv(.A(A),.Y(Y));
  `endif
+endmodule
+
+module delaygate #(parameter N=4) (input A, output Z);
+    // N must be an odd number greater than 1
+    wire [N-1:0] X;
+    wire [N:0] Y;
+    invgate inv[N:0](.A({X[N-1:0],A}),.Y(Y[N:0]));
+    assign X[N-1:0]=Y[N-1:0];
+    assign Z=Y[N];
 endmodule
 
 module andgate(input A,B, output Y);
   `ifdef COCOTB_SIM
-  assign #1 Z = A&B;
+  assign #1 Y = A&B;
   `else
   sky130_fd_sc_hd__and2_2 and2(.A(A),.B(B),.Y(Y));
   `endif
@@ -34,15 +39,15 @@ module orgate(input A,B, output Y);
   `endif
 endmodule
 
-module posedge_detector(input A, output Z);
-    delaygate dg(.A(A),.Y(Ad));
+module posedge_detector #(parameter N=4) (input A, output Z);
+    delaygate #(.N(N)) dg(.A(A),.Z(Ad));
     andgate ag(.A(A),.B(~Ad),.Y(Z));
 endmodule
 
-module clkgen_2x(input clk, output clk2x);
-    posedge_detector pdp(.A(clk),.Z(pe));
-    posedge_detector pdn(.A(~clk),.Z(ne));
-    orgate og(.A(pe),.B(ne),.Z(clk2x));
+module clkgen_2x #(parameter N=4) (input clk, output clk2x);
+    posedge_detector #(.N(N)) pdp(.A(clk),.Z(pe));
+    posedge_detector #(.N(N)) pdn(.A(~clk),.Z(ne));
+    orgate og(.A(pe),.B(ne),.Y(clk2x));
 endmodule
 
 module dff(input d,rst_n,clk, output q);
@@ -63,7 +68,18 @@ module dff(input d,rst_n,clk, output q);
   `endif
 endmodule
 
-module tt_um_example (
+module ddr_input #(parameter N=4) (input rst_n, clk, d, output q);
+
+  clkgen_2x #(.N(N)) clkgen_2x(.clk(clk),.clk2x(clk2x));
+  dff                dff(
+     .rst_n(rst_n),
+     .d(d),
+     .clk(clk2x),
+     .q(q));
+
+endmodule
+
+module ericsmi_tt_um_ddr_input_test (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -74,14 +90,16 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  assign uo_out[7:2] = {clk,clk2x,4'b1100}; 
+  assign uo_out[7:5] = {clk,2'b11}; 
   assign uio_out = 0;
   assign uio_oe  = 0;
 
-  clkgen_2x clkgen_2x(.clk(clk),.clk2x(clk2x));
+  dff d0(.d(ui_in[4]),.rst_n(rst_n),.clk(clk),.q(uo_out[4]));
 
-  dff d0(.d(ui_in[0]),.rst_n(rst_n),.clk(clk),.q(uo_out[0]));
-  dff d1(.d(ui_in[1]),.rst_n(rst_n),.clk(clk2x),.q(uo_out[1]));
+  ddr_input #(.N(1)) ddr_input1( .rst_n(rst_n), .d(ui_in[0]), .clk(clk), .q(uo_out[0]) );
+  ddr_input #(.N(3)) ddr_input3( .rst_n(rst_n), .d(ui_in[1]), .clk(clk), .q(uo_out[1]) );
+  ddr_input #(.N(5)) ddr_input5( .rst_n(rst_n), .d(ui_in[2]), .clk(clk), .q(uo_out[2]) );
+  ddr_input #(.N(7)) ddr_input7( .rst_n(rst_n), .d(ui_in[3]), .clk(clk), .q(uo_out[3]) );
     
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0};
